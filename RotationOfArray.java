c3ee0fea5d34403c9618f5fe3c759647896f7fb3
Mohd Yusuf Beig <mohdyusufbeig786@gmail.com>
2025-10-15 22:56:36 +0530

    second one

--- RotationOfArray.java
+++ RotationOfArray.java
@@ -30,6 +30,7 @@ public class RotationOfArray {
         reverseArray(arr, 0, n-1);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
+            System.out.println("hello");
         }
 
     }
